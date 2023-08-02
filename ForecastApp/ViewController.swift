9d4ec839cfc599c36d5112638bb02f9063522983
HudahanAltn <dev.hudahanaltun@gmail.com>
2023-08-02 22:13:52 +0300

    anasayfa UI tasarımı yapıldı ve anasayfa üzerindeki view'lar kendi ViewController'ına bağlandı.

--- ForecastApp/ViewController.swift
+++ ForecastApp/ViewController.swift
@@ -9,6 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    
+    @IBOutlet weak var weatherConditionImageView: UIImageView!
+    
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    @IBOutlet weak var todayLabel: UILabel!
+    
+    @IBOutlet weak var temperatureLabel: UILabel!
+    
+    @IBOutlet weak var temperatureUnderline: UILabel!
+    
+    @IBOutlet weak var weatherTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
