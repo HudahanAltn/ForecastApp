1a7c829ae1f3f8889b8b9abfd76ec2099cbdf9f6
HudahanAltn <dev.hudahanaltun@gmail.com>
2023-08-02 22:24:04 +0300

    tableviewcell view'ları kendi sınıflarına bağlandı."weatherTableView" üzerinde UI işlemler gerçekleştirildi ve ön işlemler tamamlandı.

--- ForecastApp/ViewController.swift
+++ ForecastApp/ViewController.swift
@@ -26,9 +26,74 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        setTableViewFeatures()
+        
     }
 
+    
 
 }
 
+extension ViewController{
+    
+    private func setTableViewFeatures(){
+        
+        weatherTableView.delegate = self
+        weatherTableView.dataSource = self
+        
+        weatherTableView.frame.size.height = UIScreen.main.bounds.height/2
+        weatherTableView.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.5)
+        weatherTableView.separatorStyle = .singleLine
+        weatherTableView.separatorColor = .white
+        weatherTableView.backgroundView = nil
+    }
+}
+
+extension ViewController:UITableViewDelegate,UITableViewDataSource{
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 7
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let weatherCell = weatherTableView.dequeueReusableCell(withIdentifier: "weatherCell",for:indexPath) as! WeatherTableViewCell
+        
+        weatherCell.dayLabel.text = "Pazartesi"
+        weatherCell.weatherConditionImageView.image = UIImage(systemName: "sun.max")
+        weatherCell.minTemperature.text = "10.4"
+        weatherCell.maxTemperature.text = "20.4"
+        
+        return weatherCell
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+       // Hücre arka plan rengini şeffaf yap
+        cell.backgroundColor = UIColor.clear
+    }
+
+    func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+       // Header arka plan rengini şeffaf yap
+       view.backgroundColor = UIColor.clear
+    }
+
+    func tableView(_ tableView: UITableView, willDisplayFooterView view: UIView, forSection section: Int) {
+       // Footer arka plan rengini şeffaf yap
+       view.backgroundColor = UIColor.clear
+    }
+    
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+        let rowHeight:CGFloat = tableView.frame.size.height/7
+        return rowHeight
+    }
+}
---
+++ ForecastApp/WeatherTableViewCell.swift
@@ -0,0 +1,36 @@
+//
+//  WeatherTableViewCell.swift
+//  ForecastApp
+//
+//  Created by Hüdahan Altun on 2.08.2023.
+//
+
+import UIKit
+
+class WeatherTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var dayLabel: UILabel!
+    
+    @IBOutlet weak var weatherConditionImageView: UIImageView!
+    
+    @IBOutlet weak var minTemperature: UILabel!
+    
+    @IBOutlet weak var minTempUnderline: UILabel!
+    
+    @IBOutlet weak var maxTemperature: UILabel!
+    
+    @IBOutlet weak var maxTempUnderline: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        minTempUnderline.textColor = .white
+        maxTempUnderline.textColor = .white
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
